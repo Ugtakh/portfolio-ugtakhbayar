c4dadd5a7854a01d8008aa50a606c4a1918bf222
Ugtakhbayar Surakhbayar <ugtakhbayars@gmail.com>
2023-01-11 17:32:03 +0800

    Add necessary packages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { SocialIcon } from "react-social-icons";
+import { Typewriter } from "react-simple-typewriter";
+import { motion } from "framer-motion";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <motion.div
+          initial={{ x: -1500 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 2 }}
+        >
+          <SocialIcon network="twitter" />
+          <SocialIcon network="google" />
+          <SocialIcon network="instagram" />
+          <SocialIcon network="youtube" />
+          <SocialIcon network="facebook" />
+        </motion.div>
+        <h1 style={{ color: "red" }}>
+          <Typewriter words={["Sain uu", "Pinecone"]} loop={false} />
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
